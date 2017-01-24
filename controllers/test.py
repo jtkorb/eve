3f24134be3762168f7955092633cea2a2177b1ac
Tim Korb <jtkorb@bikmort.com>
2017-01-24 11:59:19 -0500

    Added files and changes to support Travis CI.

---
+++ controllers/test.py
@@ -0,0 +1,14 @@
+def test_one(a, b):
+    '''
+
+    >>> compute_thing(3, 5):
+    8
+    '''
+    return a + b
+
+def test_two(a, b):
+    '''
+    >>> failure_thing(a, b):
+    9
+    '''
+    return 0
