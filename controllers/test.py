b9bf6e1948b9be500f741d1a789d1cd20b759ceb
Tim Korb <jtkorb@bikmort.com>
2017-01-24 12:02:52 -0500

    Temporarily fixed test functions: one succeeds, one fails.

--- controllers/test.py
+++ controllers/test.py
@@ -1,14 +1,14 @@
 def test_one(a, b):
     '''
 
-    >>> compute_thing(3, 5):
+    >>> test_one(3, 5)
     8
     '''
     return a + b
 
 def test_two(a, b):
     '''
-    >>> failure_thing(a, b):
+    >>> test_two(4, 6)
     9
     '''
     return 0
